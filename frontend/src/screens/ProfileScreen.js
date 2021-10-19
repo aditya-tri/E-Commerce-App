6b16ccc6b8f1173943a34744d567eed27367c522
Aditya <adityatrisaktisilalahi@gmail.com>
2021-10-19 16:24:58 +0700

    adding update userprofile feature

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,20 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { detailsUser } from '../actions/userActions';
+import { detailsUser, updateUserProfile } from '../actions/userActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants';
 
 export default function ProfileScreen() {
-  const userSignin = useSelector((state)=> state.userSignin)
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const userSignin = useSelector((state)=> state.userSignin);
   const { userInfo } = userSignin;
-  const userDetails = useSelector((state)=> state.userDetails)
+  const userDetails = useSelector((state)=> state.userDetails);
   const { loading, error, user } = userDetails;
+  const userUpdateProfile = useSelector((state)=> state.userUpdateProfile);
+  const {
+    success: successUpdate,
+    error: errorUpdate,
+    loading: loadingUpdate,
+  } = userUpdateProfile;
   const dispatch = useDispatch();
   useEffect(()=>{
-    dispatch(detailsUser(userInfo._id))
-  }, [dispatch, userInfo._id])
+    if(!user){
+      dispatch({ type: USER_UPDATE_PROFILE_RESET})
+      dispatch(detailsUser(userInfo._id))
+    } else{
+      setName(user.name)
+      setEmail(user.email)
+    }
+  }, [dispatch, userInfo._id, user])
   const submitHandler = (e)=>{
     e.preventDefault();
+    if(password !== confirmPassword ) {
+      alert('Password does not match!');
+    } else {
+      dispatch(updateUserProfile({ userId: user._id, name, email, password}));
+    }
   }
   return (
     <div>
@@ -28,13 +51,21 @@ export default function ProfileScreen() {
             <MessageBox variant="danger">{error}</MessageBox>
           ) : (
             <>
+            {loadingUpdate && <LoadingBox></LoadingBox>}
+            {errorUpdate && (
+              <MessageBox variant="danger">{errorUpdate}</MessageBox>
+            )}
+            {successUpdate && (
+              <MessageBox variant="success">Profile Updated Successfully</MessageBox>
+            )}
               <div>
                 <label htmlFor="name">Name</label>
                 <input
                   id="name"
                   type="text"
                   placeholder="Enter Name"
-                  value={user.name} />
+                  value={name}
+                  onChange={(e)=> setName(e.target.value)} />
               </div>
               <div>
                 <label htmlFor="email">Email</label>
@@ -42,21 +73,24 @@ export default function ProfileScreen() {
                   id="email"
                   type="email"
                   placeholder="Enter Email"
-                  value={user.email} />
+                  value={email}
+                  onChange={(e)=> setEmail(e.target.value)} />
               </div>
               <div>
                 <label htmlFor="password">Password</label>
                 <input
                   id="password"
                   type="password"
-                  placeholder="Enter Password" />
+                  placeholder="Enter Password"
+                  onChange={(e)=> setPassword(e.target.value)}/>
               </div>
               <div>
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input
                   id="confirmPassword"
                   type="password"
-                  placeholder="Enter Confirm Password"/>
+                  placeholder="Enter Confirm Password"
+                  onChange={(e)=> setConfirmPassword(e.target.value)}/>
               </div>
               <div>
                 <label htmlFor=""></label>
