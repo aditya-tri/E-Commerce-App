ec608363ea390ca98cdcaeecbd5c494205353491
Aditya <adityatrisaktisilalahi@gmail.com>
2021-10-26 09:58:08 +0700

    creating admin view feature

--- backend/utils.js
+++ backend/utils.js
@@ -2,12 +2,12 @@ import jwt from 'jsonwebtoken'
 
 export const generateToken = (user)=>{
   return jwt.sign(
-    { 
-      _id: user._id, 
+    {
+      _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-    }, 
+    },
     process.env.JWT_SECRET || 'private',
     {
       expiresIn: '30d'
@@ -20,7 +20,7 @@ export const isAuth = (req, res, next) => {
   if(authorization){
     const token = authorization.slice(7, authorization.length);
     jwt.verify(
-      token, process.env.JWT_SECRET || 'private', 
+      token, process.env.JWT_SECRET || 'private',
       (err, decode)=>{
         if(err){
           res.send(401).send({message: 'Invalid Token'})
@@ -32,5 +32,13 @@ export const isAuth = (req, res, next) => {
     );
   } else{
     res.status(401).send({message: 'No Token'})
+  }
+}
+
+export const isAdmin = (req, res, next) => {
+  if(req.user && req.user.isAdmin){
+    next();
+  } else {
+    res.status(401).send({message: 'Invalid Admin Token'})
   }
 }
